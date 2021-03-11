2397ad5c363bda9b5b2e3b1ecd97cd5c6bcece66
abhijit-kar <reachme@abhijit-kar.com>
2021-03-11 19:35:06 +0530

    Tailwind Included using Postcss & PurgeCSS added

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import postcss from 'rollup-plugin-postcss'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -37,6 +38,7 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		postcss({ extract: "tailwind.css" }),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
