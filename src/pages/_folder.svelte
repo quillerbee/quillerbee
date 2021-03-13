641d875b6bbf9c129e248af9e1388b38a5953ce3
abhijit-kar <reachme@abhijit-kar.com>
2021-03-13 18:12:36 +0530

    Separated Header to Sub Routes

--- src/pages/_folder.svelte
+++ src/pages/_folder.svelte
@@ -2,8 +2,6 @@
   import { onMount } from "svelte";
   import { url, goto, isChangingPage } from "@roxi/routify";
 
-  import Typewriter from "typewriter-effect/dist/core";
-
   // https://codepen.io/hakimel/pen/bzrZGo
   const STAR_COUNT = (window.innerWidth + window.innerHeight) / 8,
     STAR_SIZE = 3,
@@ -32,41 +30,6 @@
     document.onmouseleave = onMouseLeave;
     resize();
     step();
-
-    let typewriter = new Typewriter(header, {
-      loop: true,
-      delay: 75
-    });
-
-    typewriter
-      .pauseFor(1000)
-      .typeString("Looking for Jobs")
-      .pauseFor(300)
-      .deleteChars(4)
-      .typeString("Remote Jobs")
-      .pauseFor(1000)
-      .deleteChars(11)
-      .typeString("Indie Gigs")
-      .pauseFor(1000)
-      .deleteChars(10)
-      .typeString("Fulltime Opportunities")
-      .pauseFor(1000)
-      .changeDeleteSpeed(1)
-      .deleteChars(30)
-      .typeString(" no further")
-      .pauseFor(1000)
-      .deleteAll()
-      .typeString(
-        'We have <span style="color: #A2CD48;">handpicked</span> them for you!'
-      )
-      .pauseFor(1000)
-      .changeDeleteSpeed(1)
-      .deleteAll()
-      .typeString(
-        'Welcome to <strong style="color: yellow;">Quiller Bee</strong>'
-      )
-      .pauseFor(1000)
-      .start();
   });
 
   generate();
@@ -369,23 +332,6 @@
       </button>
     </div>
   </nav>
-  <section class="absolute top-0 flex flex-col justify-around w-full h-full p-10 pointer-events-none">
-    <div class="relative max-w-3xl px-6 mx-auto">
-      <div class="absolute top-0 left-0 flex items-center h-10 pl-10 mt-1">
-        <svg class="w-4 h-4 text-gray-600 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8
-            14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
-        </svg>
-      </div>
-      <input id="search-toggle" type="search" placeholder="Enter search term ('/' to focus)"
-        class="inline-block w-full py-3 pl-12 pr-4 mb-1 font-bold text-gray-700 bg-gray-100 rounded-lg pointer-events-auto focus:outline-none focus:ring-4 focus:ring-white focus:ring-opacity-20" />
-      <label for="search-toggle" class="absolute text-xs italic font-semibold text-white -bottom-4 right-8">
-        Powered by Dgraph
-      </label>
-    </div>
-    <h1 bind:this={header} id="typewriter"
-      class="text-lg font-bold text-white pointer-events-none sm:text-2xl md:text-4xl bottom-36">&nbsp;</h1>
-  </section>
   <canvas class="w-full h-full" bind:this={canvas} />
   <svg class="absolute bottom-0 pointer-events-none" viewBox="0 0 1428 174" version="1.1"
     xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
--- src/pages/jobs/_folder.svelte
+++ src/pages/jobs/_folder.svelte
@@ -1,6 +1,65 @@
 <script>
-    import { url } from '@roxi/routify'
+    import { onMount } from "svelte";
+    import { url } from "@roxi/routify";
+    import Typewriter from "typewriter-effect/dist/core";
+
+    let header;
+
+    onMount(() => {
+        let typewriter = new Typewriter(header, {
+            loop: true,
+            delay: 75
+        });
+
+        typewriter
+            .pauseFor(1000)
+            .typeString("Looking for Jobs")
+            .pauseFor(300)
+            .deleteChars(4)
+            .typeString("Remote Jobs")
+            .pauseFor(1000)
+            .deleteChars(11)
+            .typeString("Indie Gigs")
+            .pauseFor(1000)
+            .deleteChars(10)
+            .typeString("Fulltime Opportunities")
+            .pauseFor(1000)
+            .changeDeleteSpeed(1)
+            .deleteChars(30)
+            .typeString(" no further")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString(
+                'We have <span style="color: #A2CD48;">handpicked</span> them for you!'
+            )
+            .pauseFor(1000)
+            .changeDeleteSpeed(1)
+            .deleteAll()
+            .typeString(
+                'Welcome to <strong style="color: yellow;">Quiller Bee</strong>'
+            )
+            .pauseFor(1000)
+            .start();
+    });
 </script>
+
+<section class="absolute top-0 flex flex-col justify-around w-full p-10 pointer-events-none h-4/5">
+    <div class="relative max-w-3xl px-6 mx-auto">
+        <div class="absolute top-0 left-0 flex items-center h-10 pl-10 mt-1">
+            <svg class="w-4 h-4 text-gray-600 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8
+            14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+            </svg>
+        </div>
+        <input id="search-toggle" type="search" placeholder="Enter search term ('/' to focus)"
+            class="inline-block w-full py-3 pl-12 pr-4 mb-1 font-bold text-gray-700 bg-gray-100 rounded-lg pointer-events-auto focus:outline-none focus:ring-4 focus:ring-white focus:ring-opacity-20" />
+        <label for="search-toggle" class="absolute text-xs italic font-semibold text-white -bottom-4 right-8">
+            Powered by Dgraph
+        </label>
+    </div>
+    <h1 bind:this={header} id="typewriter"
+        class="text-lg font-bold text-white pointer-events-none sm:text-2xl md:text-4xl bottom-36">&nbsp;</h1>
+</section>
 
 <nav>
     <section class="mb-10 text-gray-900 beehive">
