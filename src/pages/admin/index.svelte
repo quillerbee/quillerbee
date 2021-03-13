cc1e0a159813ad9150f0f3388f8f299327250809
abhijit-kar <reachme@abhijit-kar.com>
2021-03-13 18:17:54 +0530

    Added Proper Headers to Routes

--- src/pages/admin/index.svelte
+++ src/pages/admin/index.svelte
@@ -1,1 +1,3 @@
-<h2>Why you should post a job here?!</h2>
+<section class="absolute top-0 flex flex-col justify-around w-full p-10 pointer-events-none h-4/5">
+    <h1 class="text-lg font-bold text-white pointer-events-none sm:text-2xl md:text-4xl bottom-36">Why Choose Us?</h1>
+</section>
