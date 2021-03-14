455dd0bcbb905412c495b70dd193b7780125a590
abhijit-kar <reachme@abhijit-kar.com>
2021-03-14 17:49:40 +0530

    Water Wave Separated

---
+++ src/components/footer/index.js
@@ -0,0 +1,1 @@
+export { default as WaterWave } from './WaterWave.svelte';
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Header } from './header/Header.svelte';
 export { default as Loader } from './Loader.svelte';
 export { default as Logo } from './Logo.svelte';
-export { default as Footer } from './Footer.svelte';
+export { default as Footer } from './footer/Footer.svelte';
