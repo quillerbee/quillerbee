b418d46fc097135bfdbe2c07c181387e057b6b63
abhijit-kar <reachme@abhijit-kar.com>
2021-03-14 12:23:47 +0530

    Remaining Navigation SVG Icons moved to Symbol Defs

--- src/pages/jobs/_folder.svelte
+++ src/pages/jobs/_folder.svelte
@@ -92,12 +92,8 @@
                     <use xlink:href="symbol-defs.svg#hex-ring" />
                 </svg>
                 <div class="absolute top-0 flex flex-col items-center w-full h-full pt-10 text-xs transform-gpu">
-                    <svg class="mb-3 text-gray-400 icon" width="90" height="35" aria-hidden="true" focusable="false"
-                        data-prefix="fas" data-icon="gamepad" role="img" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 512">
-                        <path fill="currentColor"
-                            d="M480.07 96H160a160 160 0 1 0 114.24 272h91.52A160 160 0 1 0 480.07 96zM248 268a12 12 0 0 1-12 12h-52v52a12 12 0 0 1-12 12h-24a12 12 0 0 1-12-12v-52H84a12 12 0 0 1-12-12v-24a12 12 0 0 1 12-12h52v-52a12 12 0 0 1 12-12h24a12 12 0 0 1 12 12v52h52a12 12 0 0 1 12 12zm216 76a40 40 0 1 1 40-40 40 40 0 0 1-40 40zm64-96a40 40 0 1 1 40-40 40 40 0 0 1-40 40z">
-                        </path>
+                    <svg class="mb-3 text-gray-400 icon" width="90" height="35">
+                        <use xlink:href="symbol-defs.svg#gamepad" />
                     </svg>
                     <div class="font-semibold text-gray-300 uppercase title">Game Dev</div>
                 </div>
@@ -113,12 +109,8 @@
                     <use xlink:href="symbol-defs.svg#hex-ring" />
                 </svg>
                 <div class="absolute top-0 flex flex-col items-center w-full h-full pt-10 text-xs transform-gpu">
-                    <svg class="mb-3 text-gray-400 icon" width="35" height="35" aria-hidden="true" focusable="false"
-                        data-prefix="fas" data-icon="laptop-house" role="img" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 512">
-                        <path fill="currentColor"
-                            d="M272,288H208a16,16,0,0,1-16-16V208a16,16,0,0,1,16-16h64a16,16,0,0,1,16,16v37.12C299.11,232.24,315,224,332.8,224H469.74l6.65-7.53A16.51,16.51,0,0,0,480,207a16.31,16.31,0,0,0-4.75-10.61L416,144V48a16,16,0,0,0-16-16H368a16,16,0,0,0-16,16V87.3L263.5,8.92C258,4,247.45,0,240.05,0s-17.93,4-23.47,8.92L4.78,196.42A16.15,16.15,0,0,0,0,207a16.4,16.4,0,0,0,3.55,9.39L22.34,237.7A16.22,16.22,0,0,0,33,242.48,16.51,16.51,0,0,0,42.34,239L64,219.88V384a32,32,0,0,0,32,32H272ZM629.33,448H592V288c0-17.67-12.89-32-28.8-32H332.8c-15.91,0-28.8,14.33-28.8,32V448H266.67A10.67,10.67,0,0,0,256,458.67v10.66A42.82,42.82,0,0,0,298.6,512H597.4A42.82,42.82,0,0,0,640,469.33V458.67A10.67,10.67,0,0,0,629.33,448ZM544,448H352V304H544Z">
-                        </path>
+                    <svg class="mb-3 text-gray-400 icon" width="35" height="35">
+                        <use xlink:href="symbol-defs.svg#laptop-house" />
                     </svg>
                     <div class="font-semibold text-gray-300 uppercase title">Remote</div>
                 </div>
@@ -134,12 +126,8 @@
                     <use xlink:href="symbol-defs.svg#hex-ring" />
                 </svg>
                 <div class="absolute top-0 flex flex-col items-center w-full h-full pt-10 text-xs transform-gpu">
-                    <svg class="mb-3 text-gray-400 icon" width="35" height="35" aria-hidden="true" focusable="false"
-                        data-prefix="fas" data-icon="paint-brush" role="img" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512">
-                        <path fill="currentColor"
-                            d="M167.02 309.34c-40.12 2.58-76.53 17.86-97.19 72.3-2.35 6.21-8 9.98-14.59 9.98-11.11 0-45.46-27.67-55.25-34.35C0 439.62 37.93 512 128 512c75.86 0 128-43.77 128-120.19 0-3.11-.65-6.08-.97-9.13l-88.01-73.34zM457.89 0c-15.16 0-29.37 6.71-40.21 16.45C213.27 199.05 192 203.34 192 257.09c0 13.7 3.25 26.76 8.73 38.7l63.82 53.18c7.21 1.8 14.64 3.03 22.39 3.03 62.11 0 98.11-45.47 211.16-256.46 7.38-14.35 13.9-29.85 13.9-45.99C512 20.64 486 0 457.89 0z">
-                        </path>
+                    <svg class="mb-3 text-gray-400 icon" width="35" height="35">
+                        <use xlink:href="symbol-defs.svg#paint-brush" />
                     </svg>
                     <div class="font-semibold text-gray-300 uppercase title">Design</div>
                 </div>
@@ -155,12 +143,8 @@
                     <use xlink:href="symbol-defs.svg#hex-ring" />
                 </svg>
                 <div class="absolute top-0 flex flex-col items-center w-full h-full pt-10 text-xs transform-gpu">
-                    <svg class="mb-3 text-gray-400 icon" width="35" height="35" aria-hidden="true" focusable="false"
-                        data-prefix="fas" data-icon="headset" role="img" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512">
-                        <path fill="currentColor"
-                            d="M192 208c0-17.67-14.33-32-32-32h-16c-35.35 0-64 28.65-64 64v48c0 35.35 28.65 64 64 64h16c17.67 0 32-14.33 32-32V208zm176 144c35.35 0 64-28.65 64-64v-48c0-35.35-28.65-64-64-64h-16c-17.67 0-32 14.33-32 32v112c0 17.67 14.33 32 32 32h16zM256 0C113.18 0 4.58 118.83 0 256v16c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16v-16c0-114.69 93.31-208 208-208s208 93.31 208 208h-.12c.08 2.43.12 165.72.12 165.72 0 23.35-18.93 42.28-42.28 42.28H320c0-26.51-21.49-48-48-48h-32c-26.51 0-48 21.49-48 48s21.49 48 48 48h181.72c49.86 0 90.28-40.42 90.28-90.28V256C507.42 118.83 398.82 0 256 0z">
-                        </path>
+                    <svg class="mb-3 text-gray-400 icon" width="35" height="35">
+                        <use xlink:href="symbol-defs.svg#headset" />
                     </svg>
                     <div class="font-semibold text-gray-300 uppercase title">Support</div>
                 </div>
