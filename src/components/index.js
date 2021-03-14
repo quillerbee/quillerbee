a3c028bc9008c636cd5a39fcdef3cca58e9b4f3e
abhijit-kar <reachme@abhijit-kar.com>
2021-03-14 17:36:39 +0530

    Footer Separated into Component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as DayAndNightToggle } from './DayAndNightToggle.svelte';
 export { default as InteractiveBackground } from './InteractiveBackground.svelte';
 export { default as Logo } from './Logo.svelte';
 export { default as SkipLink } from './SkipLink.svelte';
+export { default as Footer } from './Footer.svelte';
