7f6b53b6a8df16f24902aafdabb66e6062c68123
abhijit-kar <reachme@abhijit-kar.com>
2021-03-14 11:39:46 +0530

    Users Icon Moved to Symbol Defs

--- src/pages/jobs/index.svelte
+++ src/pages/jobs/index.svelte
@@ -28,9 +28,8 @@
                             Dgraph
                             <div class="flex ml-2 text-gray-400">
                                 <svg class="mr-1 fill-current inline-inline-block" width="15" height="15"
-                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                                    fill="currentColor">
+                                    <use xlink:href="symbol-defs.svg#users" />
                                 </svg>
                                 51 - 200
                             </div>
@@ -210,9 +209,8 @@
                             Dgraph
                             <div class="flex ml-2 text-gray-500">
                                 <svg class="mr-1 fill-current inline-inline-block" width="15" height="15"
-                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                                    fill="currentColor">
+                                    <use xlink:href="symbol-defs.svg#users" />
                                 </svg>
                                 51 - 200
                             </div>
@@ -336,9 +334,8 @@
                             Dgraph
                             <div class="flex ml-2 text-gray-400">
                                 <svg class="mr-1 fill-current inline-inline-block" width="15" height="15"
-                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                    <path
-                                        d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                                    fill="currentColor">
+                                    <use xlink:href="symbol-defs.svg#users" />
                                 </svg>
                                 51 - 200
                             </div>
