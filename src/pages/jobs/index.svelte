47c92a341cdbdb34a43712fe14456bb31845f3bc
abhijit-kar <reachme@abhijit-kar.com>
2021-03-14 11:51:33 +0530

    Flag Icon Moved to Symbol Defs

--- src/pages/jobs/index.svelte
+++ src/pages/jobs/index.svelte
@@ -100,11 +100,8 @@
             <div class="flex">
                 <button onclick="this.blur();"
                     class="focus:outline-none focus:ring-2 focus:ring-purple-400 p-1.5 mr-1 rounded-full cursor-pointer hover:bg-purple-200">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                        fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-                            clip-rule="evenodd" />
+                    <svg width="24" height="24">
+                        <use xlink:href="symbol-defs.svg#flag" />
                     </svg>
                 </button>
                 <button onclick="this.blur();"
@@ -269,11 +266,8 @@
             <div class="flex">
                 <button onclick="this.blur();"
                     class="focus:outline-none focus:ring-2 focus:ring-purple-400 p-1.5 mr-1 rounded-full cursor-pointer hover:bg-purple-200">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                        fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-                            clip-rule="evenodd" />
+                    <svg width="24" height="24">
+                        <use xlink:href="symbol-defs.svg#flag" />
                     </svg>
                 </button>
                 <button onclick="this.blur();"
@@ -384,11 +378,8 @@
             <div class="flex">
                 <button onclick="this.blur();"
                     class="focus:outline-none focus:ring-2 focus:ring-purple-400 p-1.5 mr-1 rounded-full cursor-pointer hover:bg-purple-200">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                        fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-                            clip-rule="evenodd" />
+                    <svg width="24" height="24">
+                        <use xlink:href="symbol-defs.svg#flag" />
                     </svg>
                 </button>
                 <button onclick="this.blur();"
