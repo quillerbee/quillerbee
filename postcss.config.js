31f719f4ab72f65942baffa7c14c84b12cdc625b
abhijit-kar <reachme@abhijit-kar.com>
2021-03-14 19:46:35 +0530

    AutoPrefixer, PostCSS Preset Env & CSS Nano Introduced

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,28 @@
 const production = !process.env.ROLLUP_WATCH;
 const purgecss = require("@fullhuman/postcss-purgecss");
 const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
+const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
     plugins: [
         require("postcss-import")(),
         tailwindcss('./tailwind.config.js'),
-        require("autoprefixer"),
-        // Only purge css on production
         production &&
         purgecss({
             content: ["./**/*.html", "./**/*.svelte"],
             defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
             safelist: [/^svelte-/]
+        }),
+        production && require("autoprefixer"),
+        production && postcssPresetEnv(),
+        production &&
+        cssnano({
+            preset: ['default', {
+                discardComments: {
+                    removeAll: true,
+                },
+            }]
         })
     ]
 };
