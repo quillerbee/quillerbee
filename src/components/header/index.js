94d3bdcd7eb3348319724a26985ce6fa5b426e3e
abhijit-kar <reachme@abhijit-kar.com>
2021-03-14 17:47:24 +0530

    CloudWave separated

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,4 @@
 export { default as DayAndNightToggle } from './DayAndNightToggle.svelte';
 export { default as InteractiveBackground } from './InteractiveBackground.svelte';
 export { default as SkipLink } from './SkipLink.svelte';
+export { default as CloudWave } from './CloudWave.svelte';
