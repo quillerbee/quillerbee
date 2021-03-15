a14eb010fae3188a900fac070c8f7290b68d1ab8
abhijit-kar <reachme@abhijit-kar.com>
2021-03-15 10:10:21 +0530

    PWA Install Button Separated & Dynamically Loaded

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -3,16 +3,12 @@
     import { goto } from "@roxi/routify";
     import { DayAndNightToggle, InteractiveBackground, SkipLink, CloudWave } from "./";
     import Logo from '../Logo.svelte';
-    import '@pwabuilder/pwainstall';
 
-    let pwaInstaller;
+    // Dynamically load component on demand (lazy load)
+    let PWAInstallBtn;
 
-    onMount(() => {
-        pwaInstaller.showopen = true;
-        pwaInstaller.installbuttontext = "Install";
-        pwaInstaller.explainer = "This app can be installed on your PC or mobile, which will allow this web app to look like any other app. You will find it in your app lists and be able to pin it as well.";
-        pwaInstaller.descriptionheader = "Quiller Bee Lite";
-    });
+    import('./PWAInstallBtn.svelte')
+        .then(result => PWAInstallBtn = result.default);
 </script>
 
 <style>
@@ -22,8 +18,7 @@
         text-align: center;
     }
 
-    .btn-hover,
-    pwa-install::part(openButton) {
+    .btn-hover {
         font-size: 16px;
         font-weight: 600;
         color: #fff;
@@ -42,8 +37,7 @@
         transition: all .4s ease-in-out;
     }
 
-    .btn-hover:hover,
-    pwa-install::part(openButton):hover {
+    .btn-hover:hover {
         background-position: 100% 0;
         moz-transition: all .4s ease-in-out;
         -o-transition: all .4s ease-in-out;
@@ -55,8 +49,7 @@
         outline: none;
     }
 
-    .btn-hover.color-3,
-    pwa-install::part(openButton) {
+    .btn-hover.color-3 {
         background-image: linear-gradient(to right, #667eea, #764ba2, #6B8DD6, #8E37D7);
         box-shadow: 0 4px 15px 0 rgba(116, 79, 168, 0.75);
     }
@@ -64,22 +57,6 @@
     .frosted-glass {
         background-color: rgba(0, 0, 0, 0.05);
         backdrop-filter: blur(10px);
-    }
-
-    pwa-install::part(openButton) {
-        position: absolute;
-        bottom: -20px;
-        right: 23%;
-    }
-
-    pwa-install::part(openButton):focus {
-        outline: none;
-
-        --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
-        --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);
-        box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
-        --tw-ring-opacity: 1;
-        --tw-ring-color: rgba(129, 140, 248, var(--tw-ring-opacity));
     }
 </style>
 
@@ -97,5 +74,8 @@
     </nav>
     <InteractiveBackground />
     <CloudWave />
-    <pwa-install bind:this={pwaInstaller}></pwa-install>
+
+    {#if PWAInstallBtn}
+    <svelte:component this={PWAInstallBtn} />
+    {/if}
 </header>
