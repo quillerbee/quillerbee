e6a2a95245a7df5d91525718b3cbc91e6e3b4774
abhijit-kar <reachme@abhijit-kar.com>
2021-03-15 13:08:47 +0530

    Showing Loader on Proper Timing

--- src/pages/_folder.svelte
+++ src/pages/_folder.svelte
@@ -1,6 +1,21 @@
 <script>
-  import { isChangingPage, metatags } from "@roxi/routify";
+  import { onMount } from "svelte";
+  import { beforeUrlChange, afterPageLoad, metatags } from "@roxi/routify";
   import { Loader, Header, Footer } from "components";
+
+  let showLoader = true;
+
+  $beforeUrlChange((event, store) => {
+    return showLoader = true;
+  });
+
+  $afterPageLoad(page => {
+    showLoader = false;
+  });
+
+  onMount(() => {
+    showLoader = false;
+  });
 
   metatags.title = `Quiller Bee`;
   metatags.description = `One stop destination for hand curated, high quality, jobs from world's most valuable companies!`;
@@ -10,7 +25,7 @@
 
 <slot />
 
-{#if $isChangingPage}
+{#if showLoader}
 <Loader />
 {/if}
 
