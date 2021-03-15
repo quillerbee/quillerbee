1de3975153d6a586a2fc585a94a5b43ed8d2eb56
abhijit-kar <reachme@abhijit-kar.com>
2021-03-15 09:55:12 +0530

    PWA Install Button Added
    Shortcuts Icon Updated

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -1,7 +1,18 @@
 <script>
+    import { onMount } from "svelte";
     import { goto } from "@roxi/routify";
     import { DayAndNightToggle, InteractiveBackground, SkipLink, CloudWave } from "./";
     import Logo from '../Logo.svelte';
+    import '@pwabuilder/pwainstall';
+
+    let pwaInstaller;
+
+    onMount(() => {
+        pwaInstaller.showopen = true;
+        pwaInstaller.installbuttontext = "Install";
+        pwaInstaller.explainer = "This app can be installed on your PC or mobile, which will allow this web app to look like any other app. You will find it in your app lists and be able to pin it as well.";
+        pwaInstaller.descriptionheader = "Quiller Bee Lite";
+    });
 </script>
 
 <style>
@@ -11,7 +22,8 @@
         text-align: center;
     }
 
-    .btn-hover {
+    .btn-hover,
+    pwa-install::part(openButton) {
         font-size: 16px;
         font-weight: 600;
         color: #fff;
@@ -30,7 +42,8 @@
         transition: all .4s ease-in-out;
     }
 
-    .btn-hover:hover {
+    .btn-hover:hover,
+    pwa-install::part(openButton):hover {
         background-position: 100% 0;
         moz-transition: all .4s ease-in-out;
         -o-transition: all .4s ease-in-out;
@@ -42,7 +55,8 @@
         outline: none;
     }
 
-    .btn-hover.color-3 {
+    .btn-hover.color-3,
+    pwa-install::part(openButton) {
         background-image: linear-gradient(to right, #667eea, #764ba2, #6B8DD6, #8E37D7);
         box-shadow: 0 4px 15px 0 rgba(116, 79, 168, 0.75);
     }
@@ -50,6 +64,22 @@
     .frosted-glass {
         background-color: rgba(0, 0, 0, 0.05);
         backdrop-filter: blur(10px);
+    }
+
+    pwa-install::part(openButton) {
+        position: absolute;
+        bottom: -20px;
+        right: 23%;
+    }
+
+    pwa-install::part(openButton):focus {
+        outline: none;
+
+        --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+        --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+        box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
+        --tw-ring-opacity: 1;
+        --tw-ring-color: rgba(129, 140, 248, var(--tw-ring-opacity));
     }
 </style>
 
@@ -67,4 +97,5 @@
     </nav>
     <InteractiveBackground />
     <CloudWave />
+    <pwa-install bind:this={pwaInstaller}></pwa-install>
 </header>
