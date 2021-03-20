69b10cda5a501cdf0b2da34823081807f581cf56
abhijit-kar <reachme@abhijit-kar.com>
2021-03-20 11:57:20 +0530

    Fixed Production Build

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 const production = !process.env.ROLLUP_WATCH;
 const tailwindcss = require("@tailwindcss/jit");
+const tailwindcssProd = require("tailwindcss");
 const cssnano = require("cssnano");
 
 module.exports = {
 	plugins: [
 		require("postcss-import")(),
-		tailwindcss(),
+		!production && tailwindcss(),
+		production && tailwindcssProd(),
 		production && require("autoprefixer"),
 		production &&
 			cssnano({
