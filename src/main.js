80399b2672c8c9a4de74715c93d11e19728c4483
abhijit-kar <reachme@abhijit-kar.com>
2021-03-23 10:47:37 +0530

    Log Level Setting Streamlined

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import App from "./App.svelte";
 import loglevel from "loglevel";
 import "./main.css";
 
-if (process.env.NODE_ENV === "production") {
-	loglevel.setLevel(loglevel.levels.ERROR);
-} else {
-	loglevel.setLevel(loglevel.levels.INFO);
-}
+loglevel.setLevel(process.env.NODE_ENV === "production" ? loglevel.levels.ERROR : loglevel.levels.INFO);
 
 window.loglevel = loglevel;
 window.log = loglevel;
