6b6f99e748c171816c518bc8884ba4a6878d0faa
abhijit-kar <reachme@abhijit-kar.com>
2021-03-30 15:07:54 +0530

    Made Job Description Form Compact

--- src/forms/JobDescription.svelte
+++ src/forms/JobDescription.svelte
@@ -149,72 +149,75 @@
 	class="relative flex flex-col px-4 py-5 mb-6 space-y-6 text-white transition-shadow duration-300 ease-in-out bg-gray-900 rounded-lg hover:shadow-lg ribbon-container">
 	<span class="ribbon left"> Job Post </span>
 
-	<div class="col-span-6 sm:col-span-3">
-		<label for="name" class="block text-sm font-medium text-gray-300"
-			>Title</label>
-		<input
-			id="title"
-			type="text"
-			name="title"
-			autocomplete="title"
-			on:change="{handleChange}"
-			on:blur="{handleChange}"
-			bind:value="{$form.title}"
-			class="{`block w-full mt-1 bg-gray-800 rounded-md shadow-sm sm:text-sm
+	<div class="grid grid-flow-col grid-cols-2 grid-rows-1 gap-x-4 gap-y-6">
+		<div>
+			<label for="name" class="block text-sm font-medium text-gray-300">
+				Title
+			</label>
+			<input
+				id="title"
+				type="text"
+				name="title"
+				autocomplete="title"
+				on:change="{handleChange}"
+				on:blur="{handleChange}"
+				bind:value="{$form.title}"
+				class="{`block w-full mt-1 bg-gray-800 rounded-md shadow-sm sm:text-sm
 			${
 				$errors.title
 					? 'border-red-500 ring-1 ring-red-500 focus:ring-red-500 focus:border-red-500'
 					: 'border-gray-700 focus:ring-indigo-500 focus:border-indigo-500'
 			}`}" />
-		{#if $errors.title}
-			<p class="mt-2 text-xs text-red-500">
-				{$errors.title}
-			</p>
-		{:else}
-			<p class="mt-2 text-xs text-gray-400">
-				Please specify as single job position like "Marketing Manager"
-				or "Node JS Developer", not a sentence like "Looking for PM /
-				Biz Dev / Manager". We know your job is important but please DO
-				NOT WRITE IN FULL CAPS. If posting multiple roles, please create
-				multiple job posts. A job post is limited to a single job. We
-				only allow real jobs.
-			</p>
-		{/if}
-	</div>
+			{#if $errors.title}
+				<p class="mt-2 text-xs text-red-500">
+					{$errors.title}
+				</p>
+			{:else}
+				<p class="hidden mt-2 text-xs text-gray-400">
+					Please specify as single job position like "Marketing
+					Manager" or "Node JS Developer", not a sentence like
+					"Looking for PM / Biz Dev / Manager". We know your job is
+					important but please DO NOT WRITE IN FULL CAPS. If posting
+					multiple roles, please create multiple job posts. A job post
+					is limited to a single job. We only allow real jobs.
+				</p>
+			{/if}
+		</div>
 
-	<div>
-		<label for="url" class="block text-sm font-medium text-gray-300">
-			Link
-		</label>
-		<div class="flex mt-1 rounded-md shadow-sm">
-			<input
-				id="url"
-				type="text"
-				name="url"
-				autocomplete="url"
-				on:change="{handleChange}"
-				on:blur="{handleChange}"
-				bind:value="{$form.url}"
-				class="{`block w-full bg-gray-800 rounded-md shadow-sm sm:text-sm
+		<div>
+			<label for="url" class="block text-sm font-medium text-gray-300">
+				Link
+			</label>
+			<div class="flex mt-1 rounded-md shadow-sm">
+				<input
+					id="url"
+					type="text"
+					name="url"
+					autocomplete="url"
+					on:change="{handleChange}"
+					on:blur="{handleChange}"
+					bind:value="{$form.url}"
+					class="{`block w-full bg-gray-800 rounded-md shadow-sm sm:text-sm
 					${
 						$errors.url
 							? 'border-red-500 ring-1 ring-red-500 focus:ring-red-500 focus:border-red-500'
 							: 'border-gray-700 focus:ring-indigo-500 focus:border-indigo-500'
 					}`}"
-				placeholder="www.example.com" />
+					placeholder="www.example.com" />
+			</div>
+			{#if $errors.url}
+				<p class="mt-2 text-xs text-red-500">
+					{$errors.url}
+				</p>
+			{:else}
+				<p class="hidden mt-2 text-xs text-gray-400">
+					Apply URLs with a form an applicant can fill out generally
+					receive a lot more applicants than having people apply by
+					email (below). A good platform to have applicants apply on
+					is Lever.co (not affiliated).
+				</p>
+			{/if}
 		</div>
-		{#if $errors.url}
-			<p class="mt-2 text-xs text-red-500">
-				{$errors.url}
-			</p>
-		{:else}
-			<p class="mt-2 text-xs text-gray-400">
-				Apply URLs with a form an applicant can fill out generally
-				receive a lot more applicants than having people apply by email
-				(below). A good platform to have applicants apply on is Lever.co
-				(not affiliated).
-			</p>
-		{/if}
 	</div>
 
 	<div>
@@ -276,7 +279,7 @@
 					$errors.salary.max}
 			</p>
 		{:else}
-			<p class="mt-2 text-xs text-gray-400">
+			<p class="hidden mt-2 text-xs text-gray-400">
 				Required because Google does not index jobs without salary data
 				since 2021! If it's a short-term gig, use the annual equivalent.
 				For example, if it's a 2-week project for $2,000, the annual
@@ -364,67 +367,68 @@
 		</div>
 	</div>
 
-	<div>
-		<label
-			for="price"
-			class="block mb-1 text-sm font-medium text-gray-300 grid-col-2">
-			Hash Tags
-		</label>
-		<div class="grid grid-flow-col gap-2">
-			<select
-				name="hashtags"
-				bind:this="{hashtagsSelector}"
-				multiple
-				class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
-			</select>
+	<div class="grid grid-flow-col grid-cols-2 grid-rows-2 gap-x-4 gap-y-6">
+		<div>
+			<label
+				for="price"
+				class="block mb-1 text-sm font-medium text-gray-300 grid-col-2">
+				Hash Tags
+			</label>
+			<div class="grid grid-flow-col gap-2">
+				<select
+					name="hashtags"
+					bind:this="{hashtagsSelector}"
+					multiple
+					class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
+				</select>
+			</div>
+		</div>
+
+		<div>
+			<label
+				for="price"
+				class="block mb-1 text-sm font-medium text-gray-300 grid-col-2"
+				>Category</label>
+			<div class="grid grid-flow-col gap-2">
+				<select
+					name="category"
+					class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
+					<option class="bg-gray-900">Software Engineer</option>
+					<option class="bg-gray-900">Game Developer</option>
+				</select>
+			</div>
+		</div>
+
+		<div>
+			<label
+				for="price"
+				class="block mb-1 text-sm font-medium text-gray-300 grid-col-2"
+				>Job Type</label>
+			<div class="grid grid-flow-col gap-2">
+				<select
+					name="type"
+					class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
+					<option class="bg-gray-900">Full Time</option>
+					<option class="bg-gray-900">Part Time</option>
+				</select>
+			</div>
+		</div>
+
+		<div>
+			<label
+				for="price"
+				class="block mb-1 text-sm font-medium text-gray-300 grid-col-2"
+				>Flair</label>
+			<div class="grid grid-flow-col gap-2">
+				<select
+					name="remote"
+					class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
+					<option class="bg-gray-900">Hot</option>
+					<option class="bg-gray-900">Urgent</option>
+				</select>
+			</div>
 		</div>
 	</div>
-
-	<div>
-		<label
-			for="price"
-			class="block mb-1 text-sm font-medium text-gray-300 grid-col-2"
-			>Category</label>
-		<div class="grid grid-flow-col gap-2">
-			<select
-				name="category"
-				class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
-				<option class="bg-gray-900">Software Engineer</option>
-				<option class="bg-gray-900">Game Developer</option>
-			</select>
-		</div>
-	</div>
-
-	<div>
-		<label
-			for="price"
-			class="block mb-1 text-sm font-medium text-gray-300 grid-col-2"
-			>Job Type</label>
-		<div class="grid grid-flow-col gap-2">
-			<select
-				name="type"
-				class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
-				<option class="bg-gray-900">Full Time</option>
-				<option class="bg-gray-900">Part Time</option>
-			</select>
-		</div>
-	</div>
-
-	<div>
-		<label
-			for="price"
-			class="block mb-1 text-sm font-medium text-gray-300 grid-col-2"
-			>Flair</label>
-		<div class="grid grid-flow-col gap-2">
-			<select
-				name="remote"
-				class="text-sm text-gray-300 bg-transparent bg-gray-800 border-gray-700 rounded-md focus:ring-indigo-500 focus:border-indigo-500">
-				<option class="bg-gray-900">Hot</option>
-				<option class="bg-gray-900">Urgent</option>
-			</select>
-		</div>
-	</div>
-
 	<div>
 		<label for="about" class="block text-sm font-medium text-gray-300">
 			Description
