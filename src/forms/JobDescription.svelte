f14f421ea68bfa5100a27a4db6939ddba990cf5e
abhijit-kar <reachme@abhijit-kar.com>
2021-04-02 15:30:15 +0530

    Preview for Markdown Added

--- src/forms/JobDescription.svelte
+++ src/forms/JobDescription.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { onMount } from "svelte";
+	import marked from "marked";
+
 	import { gql, mutation } from "@urql/svelte";
 
 	import tippy from "sveltejs-tippy";
@@ -673,7 +675,7 @@
 			</div>
 		</div>
 	</div>
-	<div>
+	<div class="relative">
 		<label
 			for="description"
 			class="inline-flex items-center text-sm font-medium text-gray-300 cursor-pointer focus:outline-none"
@@ -695,32 +697,39 @@
 				<use xlink:href="#information-circle"></use>
 			</svg>
 		</label>
-		<textarea
-			id="description"
-			name="description"
-			rows="8"
-			bind:this="{textarea}"
-			bind:value="{$data.description}"
-			class="{`block w-full mt-1 bg-gray-800 rounded-md shadow-sm sm:text-sm
+		<div class="mt-1">
+			<textarea
+				id="description"
+				name="description"
+				rows="8"
+				bind:this="{textarea}"
+				bind:value="{$data.description}"
+				class="{`w-full bg-gray-800 rounded-md shadow-sm sm:text-sm
 				${
 					$errors.description
 						? 'border-red-500 ring-1 ring-red-500 focus:ring-red-500 focus:border-red-500'
 						: 'border-gray-700 focus:ring-indigo-500 focus:border-indigo-500'
 				}
-				${
-					isEditMode ? 'rounded-bl-none' : ''
-				}`}"
-			placeholder="Describe the Job Eloquently."></textarea>
+				${isEditMode ? 'rounded-bl-none block' : 'hidden'}`}"
+				placeholder="Describe the Job Eloquently."></textarea>
+			<div
+				class="{`max-h-[50vh] overflow-auto prose px-3 py-2 min-w-full bg-white rounded-md shadow-sm sm:text-sm border-gray-700 focus:ring-indigo-500 focus:border-indigo-500
+			${!isEditMode ? 'block' : 'hidden'}`}">
+				{@html marked($data.description)}
+			</div>
+		</div>
 		<div
 			class="flex items-center justify-between text-xs text-gray-500 border-gray-700 rounded-b-md">
-			<div class="flex">
+			<div class="absolute left-0 flex bottom-[-32px] z-1">
 				<button
 					on:click|preventDefault="{() => {
 						isEditMode = true;
 					}}"
 					class="{`flex items-center p-2 focus:outline-none
 					${
-						isEditMode ? 'text-gray-400 mt-[-3px] bg-gray-800 border border-t-0 border-gray-700 rounded-b-md' : ''
+						isEditMode
+							? 'text-gray-400 bg-gray-800 border border-t-0 border-gray-700 rounded-b-md'
+							: ''
 					}`}">
 					<svg class="mr-1" width="15" height="15">
 						<use xlink:href="#pencil"></use>
@@ -733,7 +742,9 @@
 					}}"
 					class="{`flex items-center p-2 focus:outline-none
 					${
-						!isEditMode ? 'text-gray-400 mt-[-3px] bg-gray-800 border border-t-0 border-gray-700 rounded-b-md': ''
+						!isEditMode
+							? 'text-gray-400 mt-[-3px] bg-gray-800 border border-t-0 border-gray-700 rounded-b-md'
+							: ''
 					}`}">
 					<svg class="mr-1" width="15" height="15">
 						<use xlink:href="#eye"></use>
@@ -741,7 +752,7 @@
 					Preview
 				</button>
 			</div>
-			<div class="flex p-2">
+			<div class="absolute right-0 flex p-2 bottom-[-32px]">
 				<div
 					class="{`font-medium flex items-center ${
 						$data.description?.match(/\w+/g)?.length > 1000
