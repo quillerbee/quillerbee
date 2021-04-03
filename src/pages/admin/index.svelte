b9e5bd90c84d6792c9045914054dcbee347a952f
abhijit-kar <reachme@abhijit-kar.com>
2021-04-03 11:08:21 +0530

    Linked Email & Site

--- src/pages/admin/index.svelte
+++ src/pages/admin/index.svelte
@@ -68,8 +68,8 @@
 			D
 		</div>
 		<h1 class="mb-3 text-3xl font-bold">Derpy Coder</h1>
-		<div class="flex mb-3 text-xs font-semibold">
-			<div class="flex mr-2">
+		<div class="flex items-center mb-3 text-xs font-semibold">
+			<div class="flex items-center mr-2">
 				<svg
 					class="mr-0.5 text-purple-400"
 					width="15"
@@ -82,9 +82,9 @@
 						d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
 						clip-rule="evenodd"></path>
 				</svg>
-				https://www.quillerbee.com
+				<a href="https://www.quillerbee.com" class="text-sm pointer-events-auto">https://www.quillerbee.com</a>
 			</div>
-			<div class="flex">
+			<div class="flex items-center">
 				<svg
 					class="mr-0.5 text-purple-400"
 					xmlns="http://www.w3.org/2000/svg"
@@ -97,7 +97,7 @@
 						d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z"
 						clip-rule="evenodd"></path>
 				</svg>
-				abhijit.kar@derpycoder.com
+				<a href="@mailto:abhijit.kar@derpycoder.com" class="text-sm pointer-events-auto">abhijit.kar@derpycoder.com</a>
 			</div>
 		</div>
 		<blockquote class="text-center">
