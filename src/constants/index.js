5133661f51b0540ccd9fa59676e7a2c2c0c46704
abhijit-kar <reachme@abhijit-kar.com>
2021-04-03 10:58:57 +0530

    Moved Constants to a Separate File

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,34 @@ export const commonTippyConfig = {
 	theme: "warn",
 	allowHTML: true,
 };
+
+export const categories = [
+	"Software Development",
+	"Game Development",
+	"Quality Assurance",
+	"Customer Support",
+	"Sales",
+	"Marketing",
+	"Design",
+	"Legal",
+];
+
+export const jobTypes = [
+	{ text: "Full Time", value: "FullTime" },
+	{ text: "Part Time", value: "PartTime" },
+	{ text: "Contract", value: "Contract" },
+	{ text: "Temporary", value: "Temporary" },
+	{ text: "Volunteer", value: "Volunteer" },
+	{ text: "Internship", value: "Internship" },
+	{ text: "Other", value: "Other" },
+];
+
+export const flairs = [
+	{ text: "$20 - Urgent", value: "Urgent" },
+	{ text: "$30 - Exclusive", value: "Exclusive" },
+	{ text: "$40 - Promoted", value: "Promoted" },
+	{ text: "$50 - Sponsored", value: "Sponsored" },
+	{ text: "$60 - Hot", value: "Hot" },
+	{ text: "$80 - Popular", value: "Popular" },
+	{ text: "$90 - Featured", value: "Featured" },
+];
--- src/forms/JobDescription.svelte
+++ src/forms/JobDescription.svelte
@@ -15,7 +15,7 @@
 	import { countries, flag, name } from "country-emoji";
 
 	import { ToggleBtn } from "@components";
-	import { commonTippyConfig } from "@constants";
+	import { commonTippyConfig, categories, jobTypes, flairs } from "@constants";
 
 	import MarkdownEditor from "@forms/MarkdownEditor.svelte";
 
@@ -29,37 +29,6 @@
 		categorySelector,
 		jobTypeSelector,
 		flairSelector;
-
-	const categories = [
-		"Software Development",
-		"Game Development",
-		"Quality Assurance",
-		"Customer Support",
-		"Sales",
-		"Marketing",
-		"Design",
-		"Legal",
-	];
-
-	const jobTypes = [
-		{ text: "Full Time", value: "FullTime" },
-		{ text: "Part Time", value: "PartTime" },
-		{ text: "Contract", value: "Contract" },
-		{ text: "Temporary", value: "Temporary" },
-		{ text: "Volunteer", value: "Volunteer" },
-		{ text: "Internship", value: "Internship" },
-		{ text: "Other", value: "Other" },
-	];
-
-	const flairs = [
-		{ text: "$20 - Urgent", value: "Urgent" },
-		{ text: "$30 - Exclusive", value: "Exclusive" },
-		{ text: "$40 - Promoted", value: "Promoted" },
-		{ text: "$50 - Sponsored", value: "Sponsored" },
-		{ text: "$60 - Hot", value: "Hot" },
-		{ text: "$80 - Popular", value: "Popular" },
-		{ text: "$90 - Featured", value: "Featured" },
-	];
 
 	onMount(() => {
 		new SlimSelect({
