b2071838f9682b72beb9e0079bfa0a9c5dc3bc29
abhijit-kar <reachme@abhijit-kar.com>
2021-04-10 15:03:47 +0530

    Fixed Minor Styling Issues

--- src/forms/JobDescription.svelte
+++ src/forms/JobDescription.svelte
@@ -490,7 +490,7 @@
 				<div
 					data-felte-reporter-tippy-for="countries"
 					aria-labelledby="countries-label"
-					class="{`grid grid-flow-row gap-2 pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
+					class="{`pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
 						${
 							$errors.location.countries
 								? 'border-red-500 ring-2 ring-red-500 focus:ring-red-500 focus:border-red-500'
@@ -526,7 +526,7 @@
 				<div
 					data-felte-reporter-tippy-for="cities"
 					aria-labelledby="cities-label"
-					class="{`grid grid-flow-row gap-2 pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
+					class="{`pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
 					${
 						$errors.location.cities
 							? 'border-red-500 ring-2 ring-red-500 focus:ring-red-500 focus:border-red-500'
@@ -627,7 +627,7 @@
 			<div
 				data-felte-reporter-tippy-for="tags"
 				aria-labelledby="tags-label"
-				class="{`grid grid-flow-row gap-2 pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
+				class="{`pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
 				${
 					$errors.tags
 						? 'border-red-500 ring-2 ring-red-500 focus:ring-red-500 focus:border-red-500'
@@ -672,7 +672,7 @@
 			<div
 				aria-labelledby="category-label"
 				data-felte-reporter-tippy-for="category"
-				class="{`grid grid-flow-row gap-2 pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
+				class="{`pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
 				${
 					$errors.category
 						? 'border-red-500 ring-2 ring-red-500 focus:ring-red-500 focus:border-red-500'
@@ -680,7 +680,6 @@
 				}`}">
 				<select
 					id="category"
-					aria-labelledby="category-label"
 					name="category"
 					bind:this="{categorySelector}"
 					bind:value="{$data.category}"
@@ -699,7 +698,7 @@
 				for="type"
 				id="type-label"
 				data-felte-reporter-tippy-trigger-for="type"
-				class="inline-flex items-center text-sm font-medium text-gray-300 cursor-pointer focus:outline-none"
+				class="inline-flex items-center text-sm font-medium text-gray-300 truncate cursor-pointer focus:outline-none"
 				use:tippy="{{
 					...commonTippyConfig,
 					content: `
@@ -718,7 +717,7 @@
 			<div
 				aria-labelledby="type-label"
 				data-felte-reporter-tippy-for="type"
-				class="{`grid grid-flow-row gap-2 pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
+				class="{`pointer-events-auto rounded-md shadow-sm mt-1 sm:text-sm
 				${
 					$errors.type
 						? 'border-red-500 ring-2 ring-red-500 focus:ring-red-500 focus:border-red-500'
@@ -763,7 +762,7 @@
 			<div
 				data-felte-reporter-tippy-for="flair"
 				aria-labelledby="flair-label"
-				class="grid grid-flow-row gap-2 mt-1">
+				class="mt-1">
 				<select
 					id="flair"
 					name="flair"
